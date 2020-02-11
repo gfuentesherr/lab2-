577798eae9331f54a0be994cacb1c1b148953056
Gustavo Fuentesherrera <gfuentesherr@odin.cs.csubak.edu>
2020-02-10 16:08:32 -0800

    Variables renamed

--- lab2.c
+++ lab2.c
@@ -1,8 +1,9 @@
 //Variant: 1TBS (OTBS)
+
 k;
 double sin(),cos();
 main() {
-    float A=0,B=0,i,j,z[1760];
+    float Speed=0,B=0,i,j,z[1760];
     char b[1760];
     printf("\x1b[2J");
     for(;;) {
@@ -12,29 +13,30 @@ main() {
             for(i=0;6.28>i;i+=0.02) {
                 float c=sin(i),
                       d=cos(j),
-                      e=sin(A),
+                      e=sin(Speed),
                       f=sin(j),
-                      g=cos(A),
-                      h=d+2,
-                      D=1/(c*h*e+f*g+5),
+                      g=cos(Speed),
+                      height=d+2,//Height controls the size of the donut 
+                      D=1/(c*height*e+f*g+5),
                       l=cos(i),
                       m=cos(B),
                       n=sin(B),
-                      t=c*h*g-f*e;
-                      int x=40+30*D*(l*h*m-t*n),
-                      y=12+15*D*(l*h*n+t*m),
-                      o=x+80*y,
-                      N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);
-                      if(22>y&&y>0&&x>0&&80>x&&D>z[o]) {
-                          z[o]=D;;;b[o]=".,-~:;=!*#$@"[N>0?N:0];
-                      }
+                      t=c*height*g-f*e;
+                int x=40+30*D*(l*height*m-t*n),
+                    y=12+15*D*(l*height*n+t*m),
+                    o=x+80*y,
+                    N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);
+                if(22>y&&y>0&&x>0&&80>x&&D>z[o]) {
+                    z[o]=D;;;b[o]=".,-~:;=!*#$@"[N>0?N:0];
+                }
             }/*#****!!-*/
         printf("\x1b[H");
         for(k=0;1761>k;k++)
             putchar(k%80?b[k]:10);
-            A+=0.04;B+=0.02;
+            Speed+=0.01;B+=0.001;//This determines the speed of the donut
     }
-}/*****####*******!!=;:~
+}
+/*****####*******!!=;:~
        ~::==!!!**********!!!==::-
          .,~~;;;========;;;:~-.
              ..,--------,*/
