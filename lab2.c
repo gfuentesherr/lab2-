fcbbc31f0bee62b8abad3fae8818b461222f6402
Gustavo Fuentesherrera <gfuentesherr@odin.cs.csubak.edu>
2020-02-11 16:04:59 -0800

    Finished Refactor and modifications made

--- lab2.c
+++ lab2.c
@@ -1,12 +1,17 @@
 //Variant: 1TBS (OTBS)
+#include<stdio.h>
+//Files needed for color Change
 
-k;
+int k;
 double sin(),cos();
-main() {
-    float Speed=0,B=0,i,j,z[1760];
-    char b[1760];
+float Speed=0,B=0,i,j,z[1760];
+char b[1760];
+void donutAnimation();//Function Declaration
+
+main() {// Main Function Begins
     printf("\x1b[2J");
-    for(;;) {
+
+    for(;;) {// Infinite For loop that keeps on going until Force Stopped
         memset(b,32,1760);
         memset(z,0,7040);
         for(j=0;6.28>j;j+=0.07)
@@ -27,14 +32,21 @@ main() {
                     o=x+80*y,
                     N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);
                 if(22>y&&y>0&&x>0&&80>x&&D>z[o]) {
-                    z[o]=D;;;b[o]=".,-~:;=!*#$@"[N>0?N:0];
+                    z[o]=D;;;b[o]="qpdbgesSCOQP"[N>0?N:0];
                 }
             }/*#****!!-*/
-        printf("\x1b[H");
-        for(k=0;1761>k;k++)
-            putchar(k%80?b[k]:10);
-            Speed+=0.01;B+=0.001;//This determines the speed of the donut
+        donutAnimation();//Function Call 
     }
+}
+void donutAnimation() {// Function Definition
+//printf("\033[0;31m]"); // red
+//printf("\033[0;37m]");// white
+printf("\033[0;35m]");//Purple
+//printf("\033[0;34m]");//Blue
+printf("\x1b[H");
+for(k=0;1761>k;k++)
+    putchar(k%80?b[k]:10);
+    Speed+=0.001;B+=0.01;//This determines the speed of the donut
 }
 /*****####*******!!=;:~
        ~::==!!!**********!!!==::-
